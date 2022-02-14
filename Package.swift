e02d466a9e5cadb9bc825d6ddb8661d4d2569210
Amit <62500804+amitnadirping@users.noreply.github.com>
2022-02-14 15:48:58 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,20 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "PingOneSDKPackage",
+    name: "PingOnePackage",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PingOneSDK",
-            targets: ["PingOneSDK"]),
+            name: "PingOnePackage",
+            targets: ["PingOnePackage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-            .binaryTarget(name: "PingOneSDK",
-                          url: "https://github.com/amitnadirping/SPM/blob/main/PingOneSDK.xcframework.1.7.0.zip",
-                          checksum: "9052ad611f6f5531c04f95c1d91193212db2382518b3a89c58834c8cb6b44b47"),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "PingOnePackage",
+            dependencies: []),
+        .testTarget(
+            name: "PingOnePackageTests",
+            dependencies: ["PingOnePackage"]),
+        .binaryTarget(
+            name: "PingOneSDK",
+            url: "https://github.com/amitnadirping/SPM/blob/main/PingOneSDK.xcframework.1.7.0.zip",
+            checksum: "f5969f3750b2367f8ecee73814363b57dae9a0a5ec37773cc0430ff3fdf7940c"),
     ]
 )
