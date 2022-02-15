bad1365fc56275ec8a3f594324c7c30a0c832aac
Amit <62500804+amitnadirping@users.noreply.github.com>
2022-02-15 16:28:00 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,10 +16,14 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
+//        .binaryTarget(
+//            name: "PingOneSDK",
+//            url: "https://github.com/amitnadirping/SPM/blob/main/PingOneSDK.xcframework.1.7.0.zip",
+//            checksum: "f330e9eb228f373f65f7040936af2db8293dc2908416f201daa80e3187cdf526"
+//        ),
         .binaryTarget(
             name: "PingOneSDK",
-            url: "https://github.com/amitnadirping/SPM/blob/main/PingOneSDK.xcframework.1.7.0.zip",
-            checksum: "f330e9eb228f373f65f7040936af2db8293dc2908416f201daa80e3187cdf526"
-        ),
+            path: "Sources/SampleApp/PingOneSDK.xcframework"
+        )
     ]
 )
