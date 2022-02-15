4fdca0b14519fcd1cef6a1e8f4810b091d8c74ea
Amit <62500804+amitnadirping@users.noreply.github.com>
2022-02-15 16:56:46 +0200

    Add files via upload

---
+++ PingOneSDKPackage/Tests/PingOneSDKPackageTests/PingOneSDKPackageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import PingOneSDKPackage
+
+final class PingOneSDKPackageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PingOneSDKPackage().text, "Hello, World!")
+    }
+}
