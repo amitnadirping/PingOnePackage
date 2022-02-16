e9f004580679b14683c4cfffd6d9a040ac5e3179
Amit <62500804+amitnadirping@users.noreply.github.com>
2022-02-16 14:47:48 +0200

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PingOneSDK",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "PingOneSDK",
+            targets: ["PingOneSDK"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "PingOneSDK",
+            url: "https://github.com/amitnadirping/PingOneSDKPackage/blob/main/PingOneSDKPackageZIP/PingOneSDK.xcframework.1.7.0.zip",
+            checksum: "c116101b1b3c9720bea6a2a97f24831b59efc4ac7544147fa41a75a4f95fb635"
+        ),
+    ]
+)
