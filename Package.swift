d9aa0dac66da976698a5968d6ff1869830cbb8f9
Amit <62500804+amitnadirping@users.noreply.github.com>
2022-06-20 13:22:54 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "PingOneSDK",
+            name: "PingOnePackage",
             targets: ["PingOneSDK"]
         ),
     ],
