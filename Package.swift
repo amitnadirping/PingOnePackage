1715671f7a24837115ba8178bb8ec154ef5d670c
Amit <62500804+amitnadirping@users.noreply.github.com>
2022-06-21 13:54:10 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PingOneSDK", 
-            path: "https://github.com/pingidentity/pingone-mobile-sdk-ios/releases/tag/v1.7.1/PingOneSDK.xcframework.1.7.1.zip",
+            path: "https://github.com/amitnadirping/PingOnePackage/releases/tag/v1.7.1/PingOneSDK.xcframework.1.7.1.zip",
             checksum: "de027118b5096812289455d7efd7075c05db7e0839f97f9dc68daa8d5824489f")
         )
     ]
