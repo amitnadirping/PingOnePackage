807371c86aa8c4378f4f4e77a01d3df5e6c22910
Amit Nadir <62500804+amitnadirping@users.noreply.github.com>
2022-07-20 15:51:47 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,17 +5,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "PingOne",
+    name: "PingOneSDK",
     products: [
         .library(
-            name: "PingOne",
-            targets: ["PingOne"]
+            name: "PingOneSDK",
+            targets: ["PingOneSDK"]
         )
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "PingOne",
+            name: "PingOneSDK",
             url: "https://assets-test.pingone.com/pingonemobile/ios-sdk/alpha/PingOneSDK.xcframework.1.7.2.zip",
             checksum: "954ed3bbe6385c9421b66a629e8705a07a3c26971c441b883eb2c0e738297da5")
     ]
